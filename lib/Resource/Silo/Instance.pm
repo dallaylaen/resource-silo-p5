e7f908f0b5b8d62e61c8948327da868f3293e2ec
Konstantin S. Uvarin <khedin@gmail.com>
2023-07-27 00:57:46 +0200

    [ref] Move silo->ctl implementation into the same file as Resource::Silo::Instance

--- lib/Resource/Silo/Instance.pm
+++ lib/Resource/Silo/Instance.pm
@@ -14,9 +14,7 @@ Resource::Silo::Instance - base resource storage class for L<Resource::Silo>.
 =cut
 
 use Carp;
-use Scalar::Util qw(reftype blessed);
-
-use Resource::Silo::Control;
+use Scalar::Util qw( blessed reftype weaken );
 
 =head2 new
 
@@ -32,16 +30,6 @@ sub new {
     $self->ctl->override( @_ )
         if @_;
     return $self;
-};
-
-=head2 C<ctl>
-
-Interface to control methods.
-
-=cut
-
-sub ctl {
-    return Resource::Silo::Control->new(shift);
 };
 
 =head2 fresh( $resource_name [, $argument ] )
@@ -126,6 +114,217 @@ sub _make_resource_accessor {
         my $key = defined $arg && !ref $arg ? $arg : '';
         $self->{rw_cache}{$name}{$key} //= $self->fresh($name, $arg);
     };
+};
+
+=head1 CONTROL INTERFACE
+
+Sometimes more fine-grained control over the container is needed than just
+fetching the resources.
+
+Since the container class can contain arbitrary resource names and
+some user-defined methods to boot, instead of polluting its namespace,
+we provide a single method, C<ctl>, that returns a temporary facade object
+that provide access to the following methods.
+
+Most of them return the facade so that they can be chained. E.g.
+
+    # somewhere in your tests
+    silo->ctl->lock->override(
+        config  => { ... },     # some fixed values
+        dbh     => sub { ... }, # return SQLite instance
+    );
+
+=head2 C<ctl>
+
+Returns the facade.
+
+B<NOTE> the facade object is weak reference to the parent object
+and thus must not be save anywhere, lest you be surprised.
+Use it and discard immediately.
+
+=cut
+
+sub ctl {
+    my $self = shift;
+    my $facade = bless \$self, 'Resource::Silo::Control';
+    weaken $$facade;
+    confess "Attempt to close over nonexistent value"
+        unless $$facade;
+    return $facade;
+};
+
+# We're declaring a different package in the same file because
+# 1) it must have access to the internals anyway and
+# 2) we want to keep the documentation close to the implementation.
+package
+    Resource::Silo::Control;
+
+use Carp;
+use Scalar::Util qw( reftype );
+
+=head2 override( name => sub { ... }, ... )
+
+Provide a set of overrides for some of the resources.
+
+This can be used e.g. in tests to mock certain external facilities.
+
+If provided value is not a subroutine reference, it will be coerced into
+a constant one returning the given value: C<name =E<gt> sub { $value };>
+
+=cut
+
+sub override {
+    my ($self, %subst) = @_;
+
+    foreach my $name (keys %subst) {
+        croak "Attempt to override unknown resource $name"
+            unless $$self->{spec}->spec($name);
+        my $init = $subst{$name};
+        $$self->{override}{$name} = (reftype $init // '') eq 'CODE'
+            ? $init
+            : sub { $init };
+        delete $$self->{rw_cache}{$name};
+    };
+
+    return $self;
+}
+
+=head2 clear_overrides
+
+Remove all overrides set by C<override> call(s).
+
+=cut
+
+sub clear_overrides {
+    my $self = shift;
+    delete $$self->{rw_cache}{$_}
+        for keys %{ $$self->{override} };
+    delete $$self->{override};
+    return $self;
+};
+
+=head2 lock
+
+Forbid initializing new resources.
+
+The cached ones instantiated so far, the ones that have been overridden,
+and the ones with the C<assume_pure> flag will still be returned.
+
+=cut
+
+sub lock {
+    my ($self) = @_;
+    $$self->{locked} = 1;
+    return $self;
+};
+
+=head2 unlock
+
+Remove the lock set by C<lock>.
+
+=cut
+
+sub unlock {
+    my $self = shift;
+    delete $$self->{locked};
+    return $self;
+};
+
+=head2 clean_cache
+
+Remove all cached resources.
+
+No cleanup is called whatsoever, but it may be added in the future.
+
+=cut
+
+sub clean_cache {
+    my $self = shift;
+    delete $$self->{rw_cache};
+    return $self;
+};
+
+=head2 set_cache
+
+Set or delete a resource in the cache.
+
+=over
+
+=item * set_cache( resource_name => [ $instance ], ... )
+
+Set a resource without argument.
+
+=item * set_cache( resource_name => [ argument => $instance, ... ], ... )
+
+Set resource with arguments. Note that the argument checks will still be applied.
+
+=item * set_cache( resource_name => { argument => $instance, ... }, ... )
+
+Ditto.
+
+=item * set_cache( resource_name => undefined, ... )
+
+Clear cache for given resource(s), regardless of arguments.
+
+=back
+
+Note that C<set_cache( resource_name =E<gt> $instance )> is invalid
+as it may not be possible to distinguish it from one of the above forms.
+
+=cut
+
+sub set_cache {
+    my ($self, %resources) = @_;
+
+    RES: for my $name (keys %resources) {
+        my $toset = $resources{$name};
+
+        my $spec = $$self->{spec}->spec($name);
+        croak "Attempt to set unknown resource '$name'"
+            unless $spec;
+
+        if (!defined $toset) {
+            delete $$self->{rw_cache}{$name};
+            next RES;
+        } elsif (ref $toset eq 'ARRAY') {
+            unshift @$toset, '' if scalar @$toset == 1;
+            $toset = { @$toset };
+        } elsif (ref $toset eq 'HASH') {
+            # do nothing
+        } else {
+            croak "set_cache value must be undef, an array, or a hash, not "
+                .(ref $toset || "a scalar value '$toset'");
+        };
+
+        for (keys %$toset) {
+            croak "Attempt to set illegal argument '$_' for resource '$name'"
+                unless $spec->{argument}->( $_ );
+            $$self->{rw_cache}{$name}{$_} = $toset->{$_};
+        }
+    };
+
+    return $self;
+}
+
+=head2 preload( \@list )
+
+Try loading all the resources that have C<preload> flag set.
+
+May be useful if e.g. a server-side application is starting and must
+check its database connection(s) before it starts handling any clients.
+
+=cut
+
+sub preload {
+    my $self = shift;
+    # TODO allow specifying resources to load
+    #      but first come up with a way of specifying arguments, too.
+
+    my $list = $$self->{spec}->{preload};
+    for my $name (@$list) {
+        my $unused = $$self->$name;
+    };
+    return $self;
 };
 
 1;
